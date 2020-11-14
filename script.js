8b8b9d712651b6d06d12c1c7a155aaccc6496e35
itscomputers <its_computers@protonmail.com>
2020-11-14 09:54:26 -0700

    update master to match current gh-pages

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 const offset = (date, direction) => {
-	if (direction === 'next') {
-		return -date.getDay() + ((date.getDay() < 5) ? 5 : 12);
-	} else if (direction === 'prev') {
-		return -date.getDay() + ((date.getDay() > 5) ? -2 : -9);
-	}
-	return 0;
+  if (direction === 'next') {
+    return -date.getDay() + ((date.getDay() < 5) ? 5 : 12);
+  } else if (direction === 'prev') {
+    return -date.getDay() + ((date.getDay() > 5) ? -2 : -9);
+  }
+  return 0;
 };
 
 Date.prototype.moveToFriday = function(direction) {
-	this.setDate(this.getDate() + offset(this, direction));
-	return this;
+  this.setDate(this.getDate() + offset(this, direction));
+  return this;
 }
 
 Date.prototype.moveToFridayThe13th = function(direction) {
-	this.moveToFriday(direction);
-	if (this.getDate() === 13) {
-		return this;
-	} else {
-		return this.moveToFridayThe13th(direction);
-	}
+  this.moveToFriday(direction);
+  if (this.getDate() === 13) {
+    return this;
+  } else {
+    return this.moveToFridayThe13th(direction);
+  }
 }
 
 const date = new Date();
 date.moveToFridayThe13th('next');
 
 const updateDateOnWindow = () => {
-	document.getElementById('date').textContent = date.toDateString();
+  document.getElementById('date').textContent = date.toDateString();
 };
 
 const moveTo = (direction) => {
-	date.moveToFridayThe13th(direction);
-	updateDateOnWindow();
+  date.moveToFridayThe13th(direction);
+  updateDateOnWindow();
 }
 
 updateDateOnWindow();
